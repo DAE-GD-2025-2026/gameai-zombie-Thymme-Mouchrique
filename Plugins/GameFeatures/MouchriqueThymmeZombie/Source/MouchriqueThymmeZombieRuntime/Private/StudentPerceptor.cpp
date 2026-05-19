058fa428eb5640e17b01a3f15ab7bbe3ec89d4bd
Thymme-Mouchrique <thymme.mouchrique@student.howest.be>
2026-05-19 18:19:47 +0200

    Resolved various issues
    Resolved various issues concerning StudentPerceptor not being picked up by VS and thus not compiling breaking the entire logic in UE5 without an obvious cause.

--- Plugins/GameFeatures/MouchriqueThymmeZombie/Source/MouchriqueThymmeZombieRuntime/Private/StudentPerceptor.cpp
+++ Plugins/GameFeatures/MouchriqueThymmeZombie/Source/MouchriqueThymmeZombieRuntime/Private/StudentPerceptor.cpp
@@ -1,26 +1,82 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "StudentPerceptor.h"
 
-
-#include "StudentPerceptor.h"
-
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Perception/AIPerceptionComponent.h"
 
 UStudentPerceptor::UStudentPerceptor()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UStudentPerceptor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
+
+	UE_LOG(LogTemp, Warning, TEXT("StudentPerceptor BeginPlay"));
+
+	UAIPerceptionComponent* PerceptionComp = GetOwner()->FindComponentByClass<UAIPerceptionComponent>();
+
+	if (!PerceptionComp)
 	{
-		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptor::OnPerceptionUpdated);
+		UE_LOG(LogTemp, Error, TEXT("No PerceptionComponent found"));
+		return;
 	}
+
+	PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptor::OnPerceptionUpdated);
+
+	UE_LOG(LogTemp, Warning, TEXT("Bound perception callback"));
 }
 
 void UStudentPerceptor::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
-	FString::Printf(TEXT("Saw Something!")));
+	if (!Actor) return;
+
+	UE_LOG(LogTemp, Warning, TEXT("Perceived Actor: %s"), *Actor->GetName());
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	if (!OwnerPawn)
+	{
+		UE_LOG(LogTemp, Error, TEXT("OwnerPawn invalid"));
+		return;
+	}
+
+	AAIController* AIController = Cast<AAIController>(OwnerPawn->GetController());
+	if (!AIController)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AIController invalid"));
+		return;
+	}
+
+	UBlackboardComponent* Blackboard = AIController->GetBlackboardComponent();
+	if (!Blackboard)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Blackboard invalid"));
+		return;
+	}
+
+	const bool bIsZombie =
+		Actor->GetName().Contains(TEXT("Zombie")) ||
+		Actor->GetName().Contains(TEXT("Runner")) ||
+		Actor->GetName().Contains(TEXT("Heavy"));
+
+	UE_LOG(LogTemp, Warning, TEXT("IsZombie: %s"), bIsZombie ? TEXT("TRUE") : TEXT("FALSE"));
+
+	if (!bIsZombie) return;
+
+	if (Stimulus.WasSuccessfullySensed())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("SETTING TargetEnemy"));
+
+		Blackboard->SetValueAsObject(TEXT("TargetEnemy"), Actor);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CLEARING TargetEnemy"));
+
+		if (Blackboard->GetValueAsObject(TEXT("TargetEnemy")) == Actor)
+		{
+			Blackboard->ClearValue(TEXT("TargetEnemy"));
+		}
+	}
 }
